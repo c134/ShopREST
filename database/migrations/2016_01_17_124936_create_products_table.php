fa492645b7e477a9408bfd7f30be623e3ac50300
c134 <crt782@gmail.com>
2016-01-19 01:36:04 +0200

    Create create_products_table migration

---
+++ database/migrations/2016_01_17_124936_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('product_name');
+            $table->string('product_description')->nullable();
+            $table->string('image_path')->nullable();
+            $table->decimal('product_price');
+            $table->integer('category_id')->unsigned();
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('transaction_purchases', function (Blueprint $table) {
+            $table->dropForeign('category_id');
+        });
+        Schema::drop('products');
+    }
+}
