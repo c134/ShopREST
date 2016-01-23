f1ef198f27e31b3b3d219816e34946b3ebe62fb8
c134 <crt782@gmail.com>
2016-01-23 04:19:09 +0200

    Created Product service

--- public/assets/angular-app/services.js
+++ public/assets/angular-app/services.js
@@ -1,8 +1,20 @@
 angular.module('dashboardServices', ['ngResource']).
     factory('Category', function ($resource) {
         return $resource('api/categories/:id', {}, {
-            all: {method: "GET", isArray: true, params:{sorting:'@sorting'}},
-            update: {method: "PUT",
+            all: {method: "GET", isArray: true, params: {sorting: '@sorting'}},
+            update: {
+                method: "PUT",
+                params: {
+                    id: "@id"
+                }
+            }
+        })
+    }).
+    factory('Product', function ($resource) {
+        return $resource('api/products/:id', {}, {
+            all: {method: "GET", isArray: true, params: {sorting: '@sorting'}},
+            update: {
+                method: "PUT",
                 params: {
                     id: "@id"
                 }
